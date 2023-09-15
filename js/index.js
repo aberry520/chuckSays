f0070619465a8cda41ffbed3e7fae6796a2ba997
Austin Berry <aberry52098@gmail.com>
2023-09-15 11:13:45 -0400

    add categories dropdown

--- js/index.js
+++ js/index.js
@@ -1,42 +1,44 @@
 "use strict";
-// const chuckQuote = document.querySelector("#chuckQuote");
-// const newQuote = document.querySelector("#newQuote");
 
-// document.addEventListener("DOMContentLoaded", function(){
-//     const fetchQuote = fetch("https://api.chucknorris.io/jokes/random?category=dev");
-//     const quoteResponse = fetchQuote.then(function(response){
-//         return response.json();
-//     });
-//     const jsonResponse = quoteResponse.then(function(data){
-//         console.log(data);
-//         showQuote(data.value, chuckQuote)
-//         return data;
-//     })
-// });
-///////////above is the same as below////////////
-document.addEventListener("DOMContentLoaded", ()=>{
-    fetch("https://api.chucknorris.io/jokes/random?category=dev")
-    .then(function(response){
-        return response.json();
+const catList = document.querySelector("#catList");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const apiUrl = "https://api.chucknorris.io/jokes/random?category=dev";
+    get(apiUrl).then(function (response) {
+        showQuote(response.value, chuckQuote);
     })
-    .then(function(data){
-        showQuote(data.value, chuckQuote)
-        console.log(data);
-        return data;
+    const catUrl = "https://api.chucknorris.io/jokes/categories"
+    get(catUrl).then(function (response) {
+        makeCatList(response);
     })
 });
 
-function showQuote(quote, element){
+function showQuote(quote, element) {
     element.innerText = quote;
 }
+function makeCatList(catArr) {
+    const createSelect = document.createElement("select");
+    catArr.map(function (category) {
+        //create an option element
+        const option = document.createElement("option");
+        //define option attributes
+        option.value = category;
+        option.text = category;
+        // append the option to the <select>
+        createSelect.appendChild(option);
+    });
+    // append the select to the form
+    catList.append(createSelect);
+};
 
-
+catList.addEventListener("submit", function () {
+    
+});
 
 
 
 
 /////////extra add button//////////////
-newQuote.addEventListener("click", function(){
+newQuote.addEventListener("click", function () {
     history.go(0);
 })
-console.log(chuckQuote)
